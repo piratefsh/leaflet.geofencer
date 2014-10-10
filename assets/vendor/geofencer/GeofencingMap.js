f0b5e8c6b09c0402c294d955a38cbc39f88fb712
Sher Minn Chong <sherminn.chong@gmail.com>
2014-10-10 18:10:17 +0800

    Change min zoom level

--- assets/vendor/geofencer/GeofencingMap.js
+++ assets/vendor/geofencer/GeofencingMap.js
@@ -12,12 +12,12 @@ GeofencingMap.prototype = {
 
         osmUrl = 'http://{s}.tiles.mapbox.com/v3/piratefsh.jl6ae25j/{z}/{x}/{y}.png';
         var osmAttrib = 'OpenStreetMap contributors';
-        var osm = new L.TileLayer(osmUrl, { minZoom: 8, maxZoom: 17, attribution: osmAttrib });
+        var osm = new L.TileLayer(osmUrl, { minZoom: 1, maxZoom: 17, attribution: osmAttrib });
         
         var map = new L.map(this.mapId, {
             contextmenu: true,
         });
-        map.setView(new L.LatLng(25.038567638374172, 121.49175156248842), 11);
+        map.setView(new L.LatLng(25.038567638374172, 121.49175156248842), 8);
         map.addLayer(osm);
 
         this.map = map;
